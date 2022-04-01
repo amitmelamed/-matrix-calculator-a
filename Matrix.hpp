e4908e4dc57795004d498ab4737457a68b1a9834
amit <amitpay1234@gmail.com>
2022-04-01 19:55:31 +0300

    First Commit

---
+++ Matrix.hpp
@@ -0,0 +1,15 @@
+//
+// Created by Amit on 01/04/2022.
+//
+/**
+ * Matrix header file
+ * classes declared:Matrix
+ */
+
+#include <vector>
+using namespace std;
+namespace zich{
+    class Matrix{
+
+    };
+}
